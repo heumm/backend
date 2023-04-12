d79a158df18e5ffae4ea051a76deb0f3b92fb062
hanheum <hanheum97@gmail.com>
2023-04-12 21:07:35 +0900

    [add]Response status code

--- src/main/java/com/hanheum/backend/global/exception/BusinessStatus.java
+++ src/main/java/com/hanheum/backend/global/exception/BusinessStatus.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum BusinessStatus {
     EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "USR0001", "이미 존재하는 이메일입니다."),
+    NOT_USER(HttpStatus.NOT_FOUND, "USR0002", "존재하지 않는 회원입니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "ETC0001","잘못된 요청입니다."),
-    OTHER_UNKNOWN(HttpStatus.INTERNAL_SERVER_ERROR, "OTH0001", "예기치 못한 오류가 발생했습니다.");
+    OTHER_UNKNOWN(HttpStatus.INTERNAL_SERVER_ERROR, "OTH0001", "예기치 못한 오류가 발생했습니다."),
+    ;
 
     private final HttpStatus status;
     private final String code;
